0f81cd5a19e1d6ee468c135cd691c8cf50420951
raman4ik <74449936+raman4ik@users.noreply.github.com>
2021-02-24 11:04:31 +0300

    lock

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const body = document.querySelector('body')
+
 window.addEventListener('scroll', function () {
 	const header = document.querySelector('header');
 	header.classList.toggle("sticky", window.scrollY > 0)
@@ -9,5 +11,10 @@ function toggleMenu() {
 	const navigation = document.querySelector('.navigation');
 	menuToggle.classList.toggle('active')
 	navigation.classList.toggle('active')
-
+	if (navigation.classList.contains('active')) {
+		body.style.overflow = 'hidden'
+	} else {
+		body.style.overflow = 'auto'
+	}
 }
+
